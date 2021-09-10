cec795e99d0671b5587fcc2fcf5e5d8f7ba19639
nyinyiz <nyinyizaw.dev@gmail.com>
2021-09-10 16:35:26 +0630

    check code:

--- app/src/main/java/com/infotech/assignment/nyinyi/omdbmoviesapp/ui/MainActivity.kt
+++ app/src/main/java/com/infotech/assignment/nyinyi/omdbmoviesapp/ui/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         makeStatusBarTransparent()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_container)) { _, insets ->
             findViewById<FrameLayout>(R.id.main_container).setMarginTop(insets.systemWindowInsetTop)
